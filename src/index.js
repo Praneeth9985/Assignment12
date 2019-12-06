e6a386e116a2224ef5a2e1ea70b474671a885670
Praneeth9985 <54765847+Praneeth9985@users.noreply.github.com>
2019-12-05 20:03:30 -0800

    Updated Name

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Products from './Products';
+
+ReactDOM.render(<Products />, document.getElementById('root'));
+
